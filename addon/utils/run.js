b956a5358833ec0978d778033d197d329cc1af38
Juan Azambuja <juan@wyeworks.com>
2015-12-08 20:23:23 -0300

    Match Ember's later API when running en prod and dev mode

--- addon/utils/run.js
+++ addon/utils/run.js
@@ -15,8 +15,24 @@ export let Run = Ember.Object.extend({
     @param {Number} delay the time to wait to execute the function
     @return {Promise} a new promise to be fulfilled after the delay
    **/
-  later: function(fn, delay) {
-    return new RSVP.Promise(resolve => run.later(() => resolve(fn()), delay));
+  later: function(...args) {
+    let firstArg = args.shift(),
+      delay = args.pop(),
+      resFunction;
+
+    if (typeof firstArg === "function") {
+      resFunction = () => firstArg.call(null, ...args);
+    } else {
+      let providedFunction = args.shift();
+
+      if (typeof providedFunction === "string") {
+        providedFunction = firstArg[providedFunction];
+      }
+
+      resFunction = () => providedFunction.call(firstArg, ...args);
+    }
+
+    return new RSVP.Promise(resolve => run.later(() => resolve(resFunction()), delay));
   }
 });
 
